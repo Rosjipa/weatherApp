8c6504a984c22f65620f1517ebbd6ddc9d10576b
JoseLuisVargas <josvarg2014@gmail.com>
2016-07-15 17:03:36 +0200

    Update dht_11.ino
    Agregando Codigo de Ethernet shield

--- dht_11.ino
+++ dht_11.ino
@@ -1,3 +1,11 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <stdlib.h>
+#include <SPI.h>
 #include "DHT.h" 
 #define DHTPIN 2 
 #define DHTTYPE DHT11 
@@ -5,10 +13,23 @@ DHT dht(DHTPIN, DHTTYPE);
 int pinLDR = A0;
 int valorLDR = 0; 
 
+byte mac[] = { 
+0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 10,2,50,59 };
+byte server[] = { 31,170,165,240 };
+IPAddress dnServer(8,8,8,8);
+// the router's gateway address:
+IPAddress gateway(10, 2, 50,1);
+
+IPAddress subnet(255, 255, 255, 0);
+EthernetClient client; 
+String readString=String(30); 
 //librerias del DHT 11
 void setup() {
-Serial.begin(9600); //Se inicia la comunicación serial 
+Ethernet.begin(mac, ip, dnServer, gateway, subnet);
 dht.begin(); //Se inicia el sensor
+delay(1000);
+Serial.begin(9600); //Se inicia la comunicación serial 
 }
 
 void loop(){
