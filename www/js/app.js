59108fb66c1d2ebdb16486a76e9491d4424315a0
rosjipa <rosjipa2013@gmail.com>
2016-07-19 16:21:48 -0500

    aplicacion

---
+++ www/js/app.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+                    var $server;
+                    $server = 'http://localhost:1280/xampp/climaunajma/www/';
+                  
+                   function Lista(){
+                           $.ajax({
+
+                               type: "get",
+                               dataType  : 'html',
+                               url: $server+"/conecta.php",
+                               data: "listaclima",
+                               success: function(data) {
+                                    $('#listaclima').html(data);
+                                }
+                           });
+                    }
+
+                 Lista();
+
+            });
